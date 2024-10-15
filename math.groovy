f8a2fb3bead5ff103703c7f882934fee300608bf
advik dhanorkar <90509140+Advik2000@users.noreply.github.com>
2024-10-15 21:18:23 +0530

    Update math.groovy

--- math.groovy
+++ math.groovy
@@ -10,10 +10,10 @@ def sub(a,b)
 
 def mul(a,b)
 {
-  echo "The product of ${a} and ${b} is${a*b}"
+  echo "The product of ${a} and ${b} is ${a*b}"
 }
 
 def div(a,b)
 {
-  echo "The division of ${a} and ${b} is${a/b}"
+  echo "The division of ${a} and ${b} is ${a/b}"
 }
